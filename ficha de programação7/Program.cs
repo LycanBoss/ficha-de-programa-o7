95a3be1a7e60ede72fc9f4ac8a99a2e6235abd75
lycan <lycan@LAPTOP-PA36KNGK>
2021-09-29 10:43:13 -0300

    Adicionar arquivos de projeto.

---
+++ ficha de programação7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ficha_de_programação7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2, numero3;
+            Console.WriteLine("Qual a altura da escada medida em cm?");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual é a altura dos degrais en cm?");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            numero3 = numero1 / numero2;
+            Console.WriteLine("A pessoa deverá subir {0} degrais para subir a escada", numero3);
+            Console.ReadLine();
+        }
+    }
+}
